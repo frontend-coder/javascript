c744e0f46e6adbb0ca93b2be0a62d32b2b5a001c
DESKTOP-DD2BR4M\Front-End Developer <frontendercode@gmail.com>
2018-05-14 12:16:39 +0300

    v.0.1.3
    протестировано и проверено - фильтры, события на мышь, клавитуру

--- app/libs/jquery/common.js
+++ app/libs/jquery/common.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+// http://jquery.page2page.ru/index.php5/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0
+
 	$("body").niceScroll({
 horizrailenabled:false
 });
@@ -228,6 +230,336 @@ document.getElementById("cont_one").innerHTML = obj['one'];
 document.getElementById("cont_two").innerHTML = obj['two'];
 document.getElementById("cont_three").innerHTML = obj['three'];
 document.getElementById("cont_foure").innerHTML = obj['foure'];
+
+
+
+
+
+// типа делаю третий калькулятор услуг
+
+
+
+
+
+
+
+
+
+// типа пытаю джаваскрипт
+var wrapblock = document.getElementById('wrapblock');
+//загнать свойства #wrapblock в переменгную
+function changeclass() {
+wrapblock.style.color = 'black';
+// меняю в  #wrapblock свойство color: на черное
+}
+// вызываю функцию через такое то время
+ setTimeout(changeclass, 5000);
+
+// function explode(){
+//   alert("Boom!");
+// }
+// setTimeout(explode, 2000);
+
+$('.f_d').css('border', '2px solid red');
+$('.f_k').css({'border':'2px solid red', 'color':'red', 'padding': '20px 15px 54px 7px'});
+//parent > child
+// $('h4 span').css('color', 'gray').html('Бомба, епт!');
+$('h4 > span').css('color', 'gray').html('Бомба, епт!');
+//parent + child
+$('h3 + p').css('color', 'gray').html('Это вторая бомба, епт!');
+
+$('a#terpilotero').attr({href:"seomans.ru", "class":"newclass", "title":"Это текст титла"});
+
+$('.bigtimo ~ p').css('color', 'gray').html('Это вторая бомба, епт!');
+// не очень понятно как это все работает
+
+// Взаимодействие с элментами по атрибутам
+
+$('.boxcurico a[href]').css('color', 'red');
+$('.boxcurico a[title]').css('color', 'blue');
+$('.boxcurico a[href="lediseo.ru"]').css('color', 'blue');
+
+$('.boxcurico a[href!="lediseo.ru"]').css('color', 'blue');
+$('.boxcurico a[href^="tell:"]').css('color', 'blue');
+//атрибут который начинается
+$('.boxcurico a[href$=".com"]').css('color', 'yellow');
+//атрибут который завершается
+
+$('.boxcurico a[href*="lediseo"]').css('color', 'blue');
+//атрибут который содержит
+
+$('.boxcurico a[data-target|="admin"]').css('color', 'yellow');
+
+$('.boxcurico a[data-target], [download]').css('color', 'red');
+//имеет одновременно несколько атрибутов : и с теми аттриббутами и другми могут бюыть
+//2 разных тега
+
+$('.boxcurico a[data-target][download]').css('color', 'red');
+//если имеет одновременно несколько атрибутов один тег
+//2 разных тега
+
+
+
+//>Простые фильтры JQUERY
+
+$('.simpleel p:first').css('color', 'red');
+$('.simpleel p:last').css('color', 'blue');
+$('.simpleel p:eq(4)').css('color', 'grey').attr({href:"seomans.ru", "class":"newclass", "title":"Это текст титла"}); // 0, 1, 2 тоесть третий
+// 5-й эллемент р в классе
+// $('*:not(p)').css('color', 'red'); //выделятся все элементы кроме р
+// $('*:not(p,h2)').css('color', 'red'); //выделятся все элементы кроме р & h2
+
+$('.simpleel_list li:odd').css('color', 'red'); //четные
+$('.simpleel_list li:even').css('color', 'blue');  //нечетные
+$('.simpleel_list li:gt(15)').css('color', '#800080');  //li идущие после 16-го тоесть 17 и так далее
+ $('.simpleel_list li:lt(3)').css('border', '1px solid grey');  //li идущие до 3-го(не включ) 0, 1, 2
+ $('.simpleel_list :header').css({'color':'red', 'border':'1px dotted blue'});  //заголовки
+ // $('*:animated').css('color', 'red');  //заголовки
+ $('.develo:hidden').css('display', 'block');  //скрытые элементы станут видимые
+
+  $('.visibl:visible').css('visibility', 'hidden');  //видимые станут невидимими
+ //  $('li:lang(ru)').css('border', '1px solid red');  //видимые станут невидимими
+
+
+// http://jquery.page2page.ru/index.php5/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0
+
+//>Фильтры по содержимому JQUERY 5 урок
+// $('p').css('visibility', 'hidden');
+// $( ".cderjanie p:contains('попытка')" ).css('color', 'red'); // содержит слово далеко
+// $( "p:contains('djom')" ).css( "text-decoration", "underline" );
+
+// $('p:emty').css('visibility', 'hidden'); //тег без содержимого
+// $('.cderjanie:has(span)').css('text-decoration', 'underline'); // h2 содержит span
+//$('span:parent').css('text-decoration', 'underline'); // ищет родителя тега span
+
+
+//>Фильтры форм JQUERY 6 урок
+
+// $('.form_input input').css({'border':'2px solid red', 'color':'red', 'padding':'5px 15px 5px 15px'});
+// $('input:button').css('border', '1px solid grey');
+//.css({'border':'2px solid red', 'color':'red', 'padding': '20px 15px 54px 7px'});
+// $('input:checkbox').css('border', '1px solid grey');
+// $('input:text').css('border', '1px solid grey');
+// $('input:password').css('border', '1px solid grey');
+// $('input:file').css('border', '1px solid grey');
+// $('input:submit').css('border', '1px solid grey');
+// $('input:reset').css('border', '1px solid grey');
+// $('input:image').css('border', '1px solid grey');
+// $('input:focus').css('border', '1px solid grey');
+// $('input:checked').css('border', '1px solid grey');
+// $('input:enabled').css('border', '1px solid grey');
+// $('input:disabled').css('border', '1px solid grey');
+
+//События мыши JQUERY   7
+// $('.mouse_text').css('border', '1px solid grey');
+$('.mouse_text_b button').click(function(){
+$('.mouse_text').css('border', '1px solid grey');
+
+   });  // йфункция которая возвражается
+// $('.mouse_text_b button').dblclick(function(){
+// $('.mouse_text').css('border', '1px solid red');
+//    });
+
+$('.mouse_text').hover(function(){
+$('.mouse_text').css('border', '1px solid red');
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 500 );
+  });
+// Устанавливает обработчик(и) двух событий: mouseenter и mouseleave:
+
+$('.button_click a').mousedown(function(event){   });
+// момент нажатия кнопки мыши и держит
+$('.button_click a').mouseup(function(event){   });
+// момент отжатия кнопки мыши и держит
+
+$('.mouse_text_b button').mouseenter(function(){
+$('.mouse_text').css({'padding':'20px 10px', 'border' : '1px solid red'});
+
+  });
+//  наезжает  на элемент появление курсора в области элемента
+$('.button_click a').mouseover(function(event){   });
+//  наезжает  на элемент появление курсора в области элемента
+
+$('.button_click a').mouseleave(function(event){   });
+$('.button_click a').mouseout(function(event){   });
+// мышь покидает область элемента лучше чем тот что ниже
+
+$('.button_click a').mousemove(function(event){   });
+// курсор движется над областью
+$('.button_click a').toggle(function(event){   });
+// Поочередно выполняет одно из нескольких заданных действий. Имеет четыре варианта использования:
+    $(".cuprinisimo li").toggle(
+      function () {
+        $(this).css({"list-style-type":"disc", "color":"blue"});
+      },
+      function () {
+        $(this).css({"list-style-type":"disc", "color":"red"});
+      },
+      function () {
+        $(this).css({"list-style-type":"", "color":""});
+      }
+    );
+
+
+//События клавиатуры JQUERY
+$('.button_click a').keydown(function(event){   });
+
+$('.button_click a').keyup(function(event){   });
+$('.button_click a').keypress(function(eventObject){
+console.log(eventObject.which);   });
+//отлавливает коды клавиши
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// События брузера и страницы JQUERY   9
+jQuery(document).ready(function() {
+
+});
+
+$(document).ready(function() {
+  // все ли элементы - теги загрузились
+});
+
+$(window).load(function() {
+  // все все ли элементы - не только теги загрузились
+});
+
+$(window).unload(function() {
+  // все все ли элементы - не только теги загрузились
+});
+
+$(window).resize(function() {
+  // все все ли элементы - не только теги загрузились
+});
+
+
+
+
+
+// Базовые события on and off JQUERY  10
+
+
+
+
+
+
+
+
+
+
+// Переменные и их свойства JQUERY
+
+
+
+
+
+
+
+
+
+
+
+
+// Практика Драм машина JQUERY  12
+
+
+
+
+
+
+// Практика Всплывающие окна JQUERY
+
+
+
+
+// Практика Табы  JQUERY 14
+
+
+
+
+
+
+
+
+
+// Практика Слайдеры JQUERY  15
+
+
+
+
+
+
+
+
+// 16 - Практика. Адаптивное меню
+
+
+// Практика Калькулятор на сайт JQUERY 17
+
+
+
+
+
+
+// Практика Коредитная карточка JQUERY 18
+
+
+
+
+
+
+
+
+
+
+
+// Основы AJAX  JQUERY
+
+
+
+
+
+
+
+// 20 - Введение в библиотеку jQuery UI
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
 
 
 
