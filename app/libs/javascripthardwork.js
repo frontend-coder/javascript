1930676be9306802d3d31d3c1cd45764dc392fa7
Oleg <frontendercode@gmail.com>
2019-11-20 23:51:59 +0200

    v.0.23
    Выполняю задания Стартуем и пишем первую программу

--- app/libs/javascripthardwork.js
+++ app/libs/javascripthardwork.js
@@ -1,3 +1,43 @@
 'use strict';
 
-var kkjj;
+// 1. Выполняю задачу: Стартуем и пишем первую программу
+
+console.info('Мое имя Олег Василенко');
+console.info('16.03.1975');
+console.info('Добро ' + 'пожаловать ' + ' на курс');
+
+// alert(2019 - 200);
+document.getElementById('one').innerHTML = 'Привет, мир';
+const oneType = document.getElementById('one');
+oneType.style.color = 'red';
+oneType.style.cssText =
+  'font-size:30px; font-weight:900; color: orange; padding:20px 10px; border:1px solid red; display:inline-block;';
+document.getElementById('two').innerHTML = 12 * 12;
+document.getElementById('two').style.cssText =
+  'font-size:30px; font-weight:900; color: orange; padding:20px 10px; border:1px solid red; display:block; margin-top:20px';
+document.querySelector('h2 span').innerHTML = 'word';
+document.querySelector('#three').innerHTML =
+  '<h3>Заголовок с произвольным текстом</h3>';
+document.querySelector('.four').innerHTML =
+  '<h4>Заголовок с произвольным текстом</h4>';
+document.querySelector('.four').style.marginTop = '30px';
+document.querySelector('.four').style.color = 'blue';
+document.querySelector('.four').style.fontSize = '45px';
+document.querySelector('.four').innerHTML +=
+  '<p>Предложение с произвольным текстом</p>';
+
+const Y1 = 6;
+const y2 = 3;
+document.querySelector('.conteiner_mnoz').innerHTML = Y1 * y2;
+document.querySelector('.conteiner_delen').innerHTML = Y1 / y2;
+const x1 = 'Hello ';
+const x2 = 5;
+document.querySelector('.conteiner_pluss').innerHTML = x1 + x2;
+const d2 = document.querySelector('.test-2').innerHTML;
+console.log(d2);
+
+const inputGoTwo = document.querySelector('.input-gotwo');
+const inputIntwo = document.querySelector('.input-intwo');
+inputGoTwo.onclick = function() {
+  document.querySelector('.input-outwo').innerHTML += `${inputIntwo.value  }<br>`;
+};
--- app/libs/javascriptlessons.js
+++ app/libs/javascriptlessons.js
@@ -1,7 +1,42 @@
 'use strict';
 
 // первый урок по изучению Джаваскрипт
-//1. Учу Стартуем и пишем первую программу
-var projectOne = 34;
-var resultOne = projectOne + 345;
-var resultOne = resultOne * 'строка для документов';
+// 1. Учу Стартуем и пишем первую программу
+
+const taskOneZagolovok = document.getElementsByClassName('block_h2');
+
+for (let i = 0; i < taskOneZagolovok.length; i += 1) {
+  taskOneZagolovok[i].style.marginBottom = '40px';
+}
+const taskOne = document.getElementById('tastone_li');
+
+taskOne.style.color = 'red';
+document.getElementById('tastone_li').innerHTML = 'Манипуляция содержимым';
+const taskTwo = document.getElementById('tastone_litwo');
+taskTwo.style.cssText =
+  'color: blue !important; background-color: yellow;width: 100px;text-align: center;';
+const projectOne = 34;
+const resultOne = projectOne + 345;
+const resultTwo = `${projectOne}строка для документов`;
+
+document.getElementById('result_ones').innerHTML = resultOne;
+
+document.getElementById('result_one').innerText = resultTwo;
+
+document.querySelector('#result_three').innerHTML =
+  ': проверка нового заполнения';
+document.querySelector('#result_foure').innerHTML = ': проверка';
+document.getElementById('result_foure').innerHTML = ': проверка перезатирания';
+
+const pictureOver = document.getElementById('picture_over');
+pictureOver.style.cssText =
+  ' background: url(https://mirpozitiva.ru/uploads/posts/2016-08/medium/1472042903_31.jpg); width: 400px; height:400px; text-align: center;';
+
+// 2.Учу Основы ввода данных
+
+const inputIn = document.querySelector('.input-in');
+const inputGo = document.querySelector('.input-go');
+
+inputGo.onclick = function() {
+  document.querySelector('.input-out').innerText = inputIn.value;
+};
